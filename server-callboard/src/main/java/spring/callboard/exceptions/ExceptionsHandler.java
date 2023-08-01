b8c0962a73732126c7a06b3dba79c870bec5da9f
kirill2884 <kirill2884@gmail.com>
2023-08-01 23:45:10 +0300

    Exception handling

---
+++ server-callboard/src/main/java/spring/callboard/exceptions/ExceptionsHandler.java
@@ -0,0 +1,58 @@
+package spring.callboard.exceptions;
+
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
+
+@ControllerAdvice
+@Slf4j
+public class ExceptionsHandler {
+	
+	@ExceptionHandler(NotFoundException.class)
+	ResponseEntity<String> notFoundHandler(NotFoundException e){
+		String errorMessage = e.getMessage();
+		log.error(errorMessage);
+		return new ResponseEntity<>(errorMessage,HttpStatus.NOT_FOUND);
+	}
+	
+	
+	@ExceptionHandler(ConstraintViolationException.class)
+	ResponseEntity<String> constraintViolationException(ConstraintViolationException e) {
+		String errorMessage = e.getConstraintViolations().stream()
+				.map(err -> err.getPropertyPath() + ": " + err.getMessage()).collect(Collectors.joining(";"));
+		log.error(errorMessage);
+		return new ResponseEntity<>(errorMessage,HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	ResponseEntity<String> methodArgumentNotValidHandler(MethodArgumentNotValidException e) {
+		String errorMessage = e.getAllErrors().stream()
+				.map(err -> err.getCodes()[0] + ": " + err.getDefaultMessage()).collect(Collectors.joining(";"));
+		log.error(errorMessage);
+		return new ResponseEntity<>(errorMessage,HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler({IllegalArgumentException.class, HttpMessageNotReadableException.class, IllegalStateException.class})
+	ResponseEntity<String> illegalArgumentHandler(RuntimeException e) {
+		String errorMessage = e.getMessage();
+		log.error(errorMessage);
+		return new ResponseEntity<>(errorMessage,HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(RuntimeException.class)
+	ResponseEntity<String> otherExceptionHandler(RuntimeException e){
+		String errorMessage = e.getMessage();
+		log.error(errorMessage);
+		return new ResponseEntity<>(errorMessage,HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+
+}
